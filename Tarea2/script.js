16a5072e6b92b83a341f66f4426308ff8ad831b4
TomasGomezX <91841511+TomasGomezX@users.noreply.github.com>
2023-08-08 18:34:30 -0500

    Add files via upload

---
+++ Tarea2/script.js
@@ -0,0 +1,29 @@
+// Función 1: Manipulación de arreglos diferente
+const modifyArray = (arr, action) => {
+    if (action === "remove") {
+      arr.pop();
+    } else if (action === "add") {
+      arr.push("New Item");
+    }
+  };
+  
+// Función 2: Sin retorno directo pero con diferente lógica
+const printValues = (value1, value2) => {
+    console.log(`Valor 1: ${value1}`);
+    console.log(`Valor 2: ${value2}`);
+};
+
+// Arreglo inicial diferente
+let myDifferentArray = [10, 20, 30, 40, 50];
+
+// Modificando el arreglo usando la función 1
+modifyArray(myDifferentArray, "remove");
+console.log("Arreglo después de remover:", myDifferentArray);
+
+// Agregando elementos al arreglo usando la función 1
+modifyArray(myDifferentArray, "add");
+console.log("Arreglo después de agregar:", myDifferentArray);
+
+// Llamando a la función de callback (función 2) directamente
+console.log("Ejecutando la función de callback:");
+printValues("Hola", "Amigo");
